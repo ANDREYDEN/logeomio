24741a75f873cfe4dd3c3ba4ba65879c4686102c
ANDREYDEN <andrey_den@ukr.net>
2019-01-14 13:08:16 -0500

    Added intersection function

--- classes.js
+++ classes.js
@@ -63,7 +63,16 @@ class Polygon {
      *      p5.Vector - coordinates of the intersection, or null if there is no intersection
      */
     static intersect(edge, line) {
-
+        let [x1, y1] = [edge[0].x, edge[0].y];
+        let [x2, y2] = [edge[1].x, edge[1].y];
+        let [x3, y3] = [line[0].x, line[0].y];
+        let [x4, y4] = [line[1].x, line[1].y];
+        // TODO handle x1 == x2 (or not)
+        let x = ((x1-x2)*(x3*y4 - x4*y3) + (x3-x4)*(x2*y1 - x1*y2))/
+                ((y1-y2)*(x3-x4) - (y3-y4)*(x1-x2));
+        let y = (x-x1)*(y1-y2)/(x1-x2) + y1;
+        if ((x1 < x < x2 || x1 > x > x2) && (y1 < y < y2 || y1 > y > y2))
+            return createVector(x, y);
     }
 
     /* FUNCTION: splits a given polygon by a given line
