188dc739a8bef1d2f70e87306017c25bb3a60d33
ANDREYDEN <andrey_den@ukr.net>
2019-02-22 14:06:23 -0500

    fixed canvas rescaling, added download

--- scripts/consts.js
+++ scripts/consts.js
@@ -3,6 +3,5 @@ const PADDING = 15;
 const TEXT_SIZE = 100;
 const MIN_AREA = 10;
 const RED = 145;
-const SCALE_FACTOR = 3;
 const WINDOW_CANVAS_RATIO = 0.85;
 let FONT;
--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,34 @@ let logo;
 
 function displayLogo() {
     let word = document.getElementById("word_input").value;
-    logo = new Logo(word);
+    let errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
     
-    logo.dividePolygons();
-    logo.fillIn();
-    resizeCanvas(width, logo.height*SCALE_FACTOR);
-    scale(SCALE_FACTOR);
-    logo.draw(true);
-    scale(1/SCALE_FACTOR);
+    if (word.length < 2 || word.length > 15) {
+        errorMessage.innerHTML = "The name should be from 2 to 15 characters long";
+    } else {
+        logo = new Logo(word);
+        scaleFactor = width / logo.width;
+        resizeCanvas(width, scaleFactor * int(logo.height), false); // f***ing floats
+        
+        // routine
+        logo.dividePolygons();
+        logo.fillIn();
+        // scale the canvas so that the logo width is reasonable
+        scale(scaleFactor);
+        logo.draw(filledOnly=true);
+    }
 
     return false;
 } 
 
 function preload() {
-    FONT = loadFont('fonts/all-things-pink/All Things Pink Skinny.ttf');
+    FONT = loadFont("fonts/all-things-pink/All Things Pink Skinny.ttf");
 }
 
 function setup() {
-    let canvas = createCanvas(WINDOW_CANVAS_RATIO*windowWidth, 500);
-    // place th canvas in a div
+    let canvas = createCanvas(WINDOW_CANVAS_RATIO*windowWidth-15, 200);
+    // place the canvas in a div
     canvas.parent("sketch");
 
     strokeWeight(0.05);
