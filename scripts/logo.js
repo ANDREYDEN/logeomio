2b61874f744d825ce70e9444a0fa981922f8f85b
ANDREYDEN <andrey_den@ukr.net>
2019-03-15 13:45:31 -0400

    preparation

---
+++ scripts/logo.js
@@ -0,0 +1,146 @@
+class Logo {
+    /**
+     * FUNCTION: constructs a Logo object
+     * ARGS: 
+     *      word: string - a word to be displayed
+     */
+    constructor(word) {
+        this.word = word;
+        let bounds = FONT.textBounds(word, 0, 0, TEXT_SIZE);
+        this.width = bounds.w + 2 * PADDING;
+        this.height = 2*bounds.h;
+        this.polygons = [new Polygon([
+            createVector(0, 0),
+            createVector(this.width, 0),
+            createVector(this.width, this.height),
+            createVector(0, this.height)
+        ])];
+    }
+
+    /* FUNCTION: initializes the polygons data structure by splitting them with random lines
+     * ARGS: 
+     *      n: int - number of lines to split the polygons with
+     */
+    addLines(n) {
+        for (let i = 0; i < n; i++) {
+            let result = [];
+            // choose 2 points on 2 different sides of the original rectangle and
+            // split some of the polygons by a line defined by these points
+            let side1 = Math.floor(random(4));
+            let side2 = Math.floor(random(4));
+            // if the sides are same, make them different
+            if (side1 == side2)
+                side1 = (side1 + 1)%4;
+            let pointOnRect = (side) => {
+                if (side == 0)
+                    return createVector(random(this.width), 0);
+                else if (side == 1)
+                    return createVector(this.width, random(this.height));
+                else if (side == 2)
+                    return createVector(random(this.width), this.height);
+                else
+                    return createVector(0, random(this.height));
+            }
+            let a = pointOnRect(side1), b = pointOnRect(side2);
+            for (let polygon of this.polygons) {
+                let [intersections, intersectedEdges] = polygon.intersectByLine(a, b);
+                result = result.concat(polygon.split(intersections, intersectedEdges));
+            }
+            this.polygons = result;
+        }
+    }
+
+    /* FUNCTION: initializes the polygons data structure by randomly dividing them in half
+     * ARGS: 
+     *      n: int - number of times to divide the polygons in half
+     */
+    dividePolygons(areaThreshold=MIN_AREA) {
+        /* TODO:
+         *      - Turn into a single iteration
+         *      - Add bigPolygons as class field
+         */
+        
+        // consider only the polygons with big area
+        let bigPolygons = this.polygons;
+        let resultingPolygons = [];
+        while (bigPolygons.length) {
+            let polygon = bigPolygons.pop();
+            let currentArea = polygon.area();
+            
+            // pick to BIGGEST edges of the polygon
+            let intersectedEdges = [0, 1]; // indecies of intersected edges
+            let edgeLengths = [0, 0];
+            for (let i = 0; i < polygon.edges.length; i++) {
+                let vertex1 = polygon.edges[i][0];
+                let vertex2 = polygon.edges[i][1];
+                let distance = vertex1.dist(vertex2);
+                if (distance > edgeLengths[0]) {
+                    edgeLengths[1] = edgeLengths[0];
+                    edgeLengths[0] = distance;
+                    intersectedEdges[1] = intersectedEdges[0];
+                    intersectedEdges[0] = i;
+                } else if (distance > edgeLengths[1]) {
+                    edgeLengths[1] = distance;
+                    intersectedEdges[1] = i;
+                }
+            }
+            
+            // pick to points on the selected edges
+            let intersections = [polygon.pickPoint(intersectedEdges[0]),
+                                 polygon.pickPoint(intersectedEdges[1])];
+
+            // split the polygon in half
+            let subPolygons = polygon.split(intersections, intersectedEdges);
+            
+            // if the selected polygon was still big enough, push its halves to bigPolygons
+            // else push the halves to the resultingPolygons
+            if (currentArea > areaThreshold) {
+                bigPolygons.push(subPolygons[0]);
+                bigPolygons.push(subPolygons[1]);
+            } else {
+                resultingPolygons.push(subPolygons[0]);
+                resultingPolygons.push(subPolygons[1]);
+            }
+        }
+        print('finished dividing');
+        this.polygons = resultingPolygons;
+    }
+
+    /* FUNCTION: fills the polygons according to the word (refreshes the canvas at the end)*/
+    fillIn(pixelDistance=1) {
+        // draw the actual word
+        fill(RED, 0, 0);
+        text(this.word, this.width / 2, this.height / 2);
+
+        // fill those polygons that intersect the word
+        loadPixels();
+        for (let y = 0; y < this.height; y += pixelDistance)
+            for (let x = 0; x < this.width; x += pixelDistance) 
+                if (pixels[(x + y * width) * 4] == RED && 
+                    pixels[(x + y * width) * 4 + 1] == 0) {
+                    for (let p of this.polygons)
+                        if (p.contains(createVector(x, y))) {
+                            p.filled = true;
+                            break;
+                        }      
+                }
+        background(255);
+    }
+
+    /* FUNCTION: draws all polygons on a p5 canvas 
+    *  ARGS:
+    *       filledOnly: bool - draw only filled polygons
+    *
+    */
+    draw(filledOnly=false) {
+        for (let polygon of this.polygons) {
+            if (filledOnly) {
+                if (polygon.filled) {
+                    polygon.draw();  
+                }
+            } else {
+                polygon.draw();  
+            }       
+        }   
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+let logo;
+
+function displayLogo() {
+    let word = document.getElementById("word_input").value;
+    let errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
+    
+    if (word.length < 2 || word.length > 15) {
+        // display error
+        errorMessage.innerHTML = "The name should be from 2 to 15 characters long";
+    } else {
+        logo = new Logo(word);
+        scaleFactor = width / logo.width;
+        resizeCanvas(width, int(scaleFactor * logo.height), false);
+        
+        // routine
+        logo.dividePolygons();
+        logo.fillIn();
+        // scale the canvas so that the logo width is reasonable
+        scale(scaleFactor);
+        logo.draw(filledOnly=true);
+    }
+
+    return false;
+} 
+
+function preload() {
+    FONT = loadFont("fonts/All_Things_Pink.ttf");
+}
+
+function setup() {
+    let canvas = createCanvas(int(WINDOW_CANVAS_RATIO*windowWidth)-15, 200);
+    // place the canvas in a div
+    canvas.parent("sketch");
+
+    strokeWeight(0.05);
+    textSize(TEXT_SIZE);
+    textFont(FONT);
+    textAlign(CENTER, CENTER);
+    pixelDensity(1);
+
+    displayLogo();
+}
+
+function draw() {
+
+}
